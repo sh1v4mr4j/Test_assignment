11cdbe588ebe81a8d8ae2f8796a7286574d2cb50
Shivam Raj <shivamrj83@gmail.com>
2019-08-03 19:21:43 +0530

    Frame ready

---
+++ index.js
@@ -0,0 +1,53 @@
+let  array=[{
+    question:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+    options:["Sed ut perspiciatis unde omnis","iste natus error sit voluptatem accusantium","quae ab illo inventore veritatis et","quasi architecto beatae vitae dicta sunt"],
+    answer:0,
+    review:false
+},{
+    question:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+    options:["Sed ut perspiciatis unde omnis","iste natus error sit voluptatem accusantium","quae ab illo inventore veritatis et","quasi architecto beatae vitae dicta sunt"],
+    answer:0,
+    review:false
+},{
+    question:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+    options:["Sed ut perspiciatis unde omnis","iste natus error sit voluptatem accusantium","quae ab illo inventore veritatis et","quasi architecto beatae vitae dicta sunt"],
+    answer:0,
+    review:false
+},{
+    question:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+    options:["Sed ut perspiciatis unde omnis","iste natus error sit voluptatem accusantium","quae ab illo inventore veritatis et","quasi architecto beatae vitae dicta sunt"],
+    answer:0,
+    review:false
+},{
+    question:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+    options:["Sed ut perspiciatis unde omnis","iste natus error sit voluptatem accusantium","quae ab illo inventore veritatis et","quasi architecto beatae vitae dicta sunt"],
+    answer:0,
+    review:false
+},{
+    question:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+    options:["Sed ut perspiciatis unde omnis","iste natus error sit voluptatem accusantium","quae ab illo inventore veritatis et","quasi architecto beatae vitae dicta sunt"],
+    answer:0,
+    review:false
+},{
+    question:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+    options:["Sed ut perspiciatis unde omnis","iste natus error sit voluptatem accusantium","quae ab illo inventore veritatis et","quasi architecto beatae vitae dicta sunt"],
+    answer:0,
+    review:false
+},{
+    question:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+    options:["Sed ut perspiciatis unde omnis","iste natus error sit voluptatem accusantium","quae ab illo inventore veritatis et","quasi architecto beatae vitae dicta sunt"],
+    answer:0,
+    review:false
+},{
+    question:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+    options:["Sed ut perspiciatis unde omnis","iste natus error sit voluptatem accusantium","quae ab illo inventore veritatis et","quasi architecto beatae vitae dicta sunt"],
+    answer:0,
+    review:false
+},{
+    question:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+    options:["Sed ut perspiciatis unde omnis","iste natus error sit voluptatem accusantium","quae ab illo inventore veritatis et","quasi architecto beatae vitae dicta sunt"],
+    answer:0,
+    review:false
+},]
+
+
